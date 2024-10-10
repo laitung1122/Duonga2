0419caa0a5e773c6bc74bae36fc66b35d50cdc90
laitung1122 <137845259+laitung1122@users.noreply.github.com>
2024-10-10 22:00:22 +0700

    Update duong.lua

--- duong.lua
+++ duong.lua
@@ -1,5 +1,4 @@
 local branch = getgenv().DuongApi_dev_mode and "dev" or "main"
-
 local HttpService = game:GetService("HttpService")
 local baseURL = "https://raw.githubusercontent.com/laitung1122/Duonga2/" .. branch
 
@@ -9,7 +8,10 @@ export type gameMapping = {
 }
 
 if not getgenv().ExecutorSupport then
-    loadstring(game:HttpGet(baseURL .. "/linh.lua"))()
+    local success, err = pcall(function()
+        loadstring(game:HttpGet(baseURL .. "/linh.lua"))()
+    end)
+    if not success then warn("Failed to load linh.lua: " .. err) end
 end
 
 if not getgenv().BloxstrapRPC then
@@ -32,11 +34,7 @@ if not getgenv().BloxstrapRPC then
     }
 
     function BloxstrapRPC.SendMessage(command: string, data: any)
-        local json = HttpService:JSONEncode({
-            command = command, 
-            data = data
-        })
-        
+        local json = HttpService:JSONEncode({command = command, data = data})
         print("[BloxstrapRPC] " .. json)
     end
 
@@ -44,137 +42,101 @@ if not getgenv().BloxstrapRPC then
         if data.timeStart ~= nil then
             data.timeStart = math.round(data.timeStart)
         end
-        
         if data.timeEnd ~= nil then
             data.timeEnd = math.round(data.timeEnd)
         end
-        
         BloxstrapRPC.SendMessage("SetRichPresence", data)
-    end 
+    end
 
     getgenv().BloxstrapRPC = BloxstrapRPC
 end
 
-local mapping: gameMapping = HttpService:JSONDecode(game:HttpGet(baseURL .. "/map/" .. game.GameId .. ".json"))
-local scriptPath = mapping.main
+local success, result = pcall(function()
+    return game:HttpGet(baseURL .. "/map/" .. game.GameId .. ".json")
+end)
+if success then
+    local mapping = HttpService:JSONDecode(result)
+    local scriptPath = mapping.main
 
-if mapping.exclusions and mapping.exclusions[tostring(game.PlaceId)] then
-    scriptPath = mapping.exclusions[tostring(game.PlaceId)]
+    if mapping.exclusions and mapping.exclusions[tostring(game.PlaceId)] then
+        scriptPath = mapping.exclusions[tostring(game.PlaceId)]
+    end
+
+    local successScript, errScript = pcall(function()
+        loadstring(game:HttpGet(baseURL .. scriptPath))()
+    end)
+    if not successScript then warn("Failed to load script: " .. errScript) end
+else
+    warn("Failed to get mapping: " .. result)
 end
 
-loadstring(game:HttpGet(baseURL .. scriptPath))()
-
-
--- Addons (this is BETA, expect stuff to change) --
 if getgenv().mspaint_disable_addons then return end
+
 task.spawn(function()
-    local supportsFileSystem = (ExecutorSupport["isfile"] and ExecutorSupport["delfile"] and ExecutorSupport["listfiles"] and ExecutorSupport["writefile"] and ExecutorSupport["makefolder"] and ExecutorSupport["isfolder"])
-    
+    local fileSystemAPIs = {"isfile", "delfile", "listfiles", "writefile", "makefolder", "isfolder"}
+    local supportsFileSystem = true
+    for _, api in ipairs(fileSystemAPIs) do
+        if not ExecutorSupport[api] then
+            supportsFileSystem = false
+            break
+        end
+    end
+
     if not supportsFileSystem then
         warn("[DuongApi] Your executor doesn't support the FileSystem API. Addons will not work.")
         return
     end
 
     if not isfolder("DuongApi/addons") then
-        print("[DuongApi] Addons folder doesn't exist. Creating...")
         makefolder("DuongApi/addons")
         return
     end
     
     repeat task.wait() until getgenv().mspaint_loaded == true
-    print("[DuongApi] Loading addons...")
 
-    -- Functions
-    local function getGameAddonPath(path: string)
+    local function getGameAddonPath(path)
         return string.match(path, "/places/(.-)%.lua")
     end
 
     local function AddAddonElement(LinoriaElement, AddonName, Element)
-        if not LinoriaElement then
-            warn("[DuongApi] Element '" .. tostring(Element.Name) .. " (" .. tostring(Element.Type) .. ")' didn't load: Invalid Linoria element.")
-            return
-        end
-
-        if typeof(Element) ~= "table" then
-            warn("[DuongApi] Element '" .. tostring(Element.Name) .. " (" .. tostring(Element.Type) .. ")' didn't load: Invalid data.")
-            return
-        end 
-
-        if typeof(Element.Type) ~= "string" then 
-            warn("[DuongApi] Element '" .. tostring(Element.Name) .. " (" .. tostring(Element.Type) .. ")' didn't load: Invalid name.")
-            return 
-        end
-
-        if typeof(AddonName) ~= "string" then 
-            warn("[DuongApi] Element '" .. tostring(Element.Name) .. " (" .. tostring(Element.Type) .. ")' didn't load: Invalid addon name.")
-            return 
-        end
-
+        if not LinoriaElement then return end
+        if typeof(Element) ~= "table" or typeof(Element.Type) ~= "string" then return end
+        if typeof(AddonName) ~= "string" then return end
         if Element.Type:sub(1, 3) == "Add" then Element.Type = Element.Type:sub(4) end
 
-        -- Elements with no Arguments
         if Element.Type == "Divider" then
             return LinoriaElement:AddDivider()
-        end
-
-        if Element.Type == "DependencyBox" then
+        elseif Element.Type == "DependencyBox" then
             return LinoriaElement:AddDependencyBox()
-        end
-
-        if typeof(Element.Name) ~= "string" then 
-            warn("[DuongApi] Element '" .. tostring(Element.Name) .. " (" .. tostring(Element.Type) .. ")' didn't load: Invalid name.")
-            return 
-        end
-
-        -- Elements with Arguments
-        if typeof(Element.Arguments) == "table" then
+        elseif typeof(Element.Arguments) == "table" then
             if Element.Type == "Label" then
                 return LinoriaElement:AddLabel(table.unpack(Element.Arguments))
-            end
-
-            if Element.Type == "Toggle" then
+            elseif Element.Type == "Toggle" then
                 return LinoriaElement:AddToggle(AddonName .. "_" .. Element.Name, Element.Arguments)
-            end
-            
-            if Element.Type == "Button" then
+            elseif Element.Type == "Button" then
                 return LinoriaElement:AddButton(Element.Arguments)
-            end
-            
-            if Element.Type == "Slider" then
+            elseif Element.Type == "Slider" then
                 return LinoriaElement:AddSlider(AddonName .. "_" .. Element.Name, Element.Arguments)
-            end
-            
-            if Element.Type == "Input" then
+            elseif Element.Type == "Input" then
                 return LinoriaElement:AddInput(AddonName .. "_" .. Element.Name, Element.Arguments)
-            end
-            
-            if Element.Type == "Dropdown" then
+            elseif Element.Type == "Dropdown" then
                 return LinoriaElement:AddInput(AddonName .. "_" .. Element.Name, Element.Arguments)
-            end
-            
-            if Element.Type == "ColorPicker" then
+            elseif Element.Type == "ColorPicker" then
                 return LinoriaElement:AddColorPicker(AddonName .. "_" .. Element.Name, Element.Arguments)        
-            end
-            
-            if Element.Type == "KeyPicker" then
+            elseif Element.Type == "KeyPicker" then
                 return LinoriaElement:AddKeyPicker(AddonName .. "_" .. Element.Name, Element.Arguments)
             end
         end
-
-        warn("[DuongApi] Element '" .. tostring(Element.Name) .. " (" .. tostring(Element.Type) .. ")' didn't load: Invalid element type.")
     end
 
     local gameAddonPath = getGameAddonPath(scriptPath)
-    print("[DuongApi] Game addon path: " .. gameAddonPath)
-    
     local AddonTab, LastGroupbox = nil, "Right"
 
-    local function createAddonTab(hasAddons: boolean)
-        if AddonTab ~= nil then return end -- tab was already created
-        local addonsText = "This tab is for UN-OFFICIAL addons made for DuongApi. We are not responsible for what addons you will use. You are putting yourself AT RISK since you are executing third-party scripts."
-        if not hasAddons then
-            addonsText = "Your addons FOLDER is empty!"
-        end
+    local function createAddonTab(hasAddons)
+        if AddonTab ~= nil then return end
+        local addonsText = hasAddons and 
+            "This tab is for UN-OFFICIAL addons made for DuongApi. We are not responsible for what addons you will use. You are putting yourself AT RISK since you are executing third-party scripts." or
+            "Your addons FOLDER is empty!"
         AddonTab = getgenv().Library.Window:AddTab("Addons [BETA]")
         AddonTab:UpdateWarningBox({
             Visible = true,
@@ -185,42 +147,25 @@ task.spawn(function()
 
     local containAddonsLoaded = false
     for _, file in pairs(listfiles("DuongApi/addons")) do
-        print("[DuongApi] Loading addon '" .. string.gsub(file, "mspaint/addons/", "") .. "'...")
         if file:sub(#file - 3) ~= ".lua" and file:sub(#file - 4) ~= ".luau" and file:sub(#file - 7) ~= ".lua.txt" then continue end
-
         local success, errorMessage = pcall(function()
             local fileContent = readfile(file)
             local addon = loadstring(fileContent)()
 
-            if typeof(addon.Name) ~= "string" or typeof(addon.Elements) ~= "table" then
-                warn("Addon '" .. string.gsub(file, "mspaint/addons/", "") .. "' didn't load: Invalid Name/Elements.")
-                return 
-            end
-
+            if typeof(addon.Name) ~= "string" or typeof(addon.Elements) ~= "table" then return end
             if typeof(addon.Game) == "string" then
-                if addon.Game ~= gameAddonPath and addon.Game ~= "*" then
-                    warn("Addon '" .. string.gsub(file, "mspaint/addons/", "") .. "' didn't load: Wrong game.")
-                    return
-                end
+                if addon.Game ~= gameAddonPath and addon.Game ~= "*" then return end
             elseif typeof(addon.Game) == "table" then
-                if not table.find(addon.Game, gameAddonPath) then
-                    warn("Addon '" .. string.gsub(file, "mspaint/addons/", "") .. "' didn't load: Wrong game.")
-                    return
-                end
-            else
-                warn("Addon '" .. string.gsub(file, "mspaint/addons/", "") .. "' didn't load: Invalid GameId.")
-                return
-            end
+                if not table.find(addon.Game, gameAddonPath) then return end
+            else return end
 
             addon.Name = addon.Name:gsub("%s+", "")
-            if typeof(addon.Title) ~= "string" then
-                addon.Title = addon.Name;
-            end
-            
+            addon.Title = typeof(addon.Title) == "string" and addon.Title or addon.Name
+
             if not AddonTab then createAddonTab(true) end
 
-            local AddonGroupbox = LastGroupbox == "Right" and AddonTab:AddLeftGroupbox(addon.Title) or AddonTab:AddRightGroupbox(addon.Title);
-            LastGroupbox = LastGroupbox == "Right" and "Left" or "Right";
+            local AddonGroupbox = LastGroupbox == "Right" and AddonTab:AddLeftGroupbox(addon.Title) or AddonTab:AddRightGroupbox(addon.Title)
+            LastGroupbox = LastGroupbox == "Right" and "Left" or "Right"
             if typeof(addon.Description) == "string" then
                 AddonGroupbox:AddLabel(addon.Description, true)
             end
@@ -228,7 +173,7 @@ task.spawn(function()
             local function loadElements(linoriaMainElement, elements)
                 for _, element in pairs(elements) do                      
                     local linoriaElement = AddAddonElement(linoriaMainElement, addon.Name, element)
-                    if linoriaElement ~= nil and typeof(element.Elements) == "table" then
+                    if linoriaElement and typeof(element.Elements) == "table" then
                         loadElements(linoriaElement, element.Elements)
                     end  
                 end
@@ -237,10 +182,10 @@ task.spawn(function()
             loadElements(AddonGroupbox, addon.Elements)
         end)
 
-        if not success then
-            warn("[DuongApi] Failed to load addon '" .. string.gsub(file, "mspaint/addons/", "") .. "':", errorMessage)
-        else
+        if success then
             containAddonsLoaded = true
+        else
+            warn("[DuongApi] Failed to load addon '" .. file .. "': " .. errorMessage)
         end
     end
     createAddonTab(containAddonsLoaded)
