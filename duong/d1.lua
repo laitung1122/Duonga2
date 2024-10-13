a2c70d10e46f6b694b1c861e713564f35b0b0e4d
laitung1122 <137845259+laitung1122@users.noreply.github.com>
2024-10-13 11:06:39 +0700

    Update d1.lua

--- duong/d1.lua
+++ duong/d1.lua
@@ -436,25 +436,7 @@ local Tabs = {
     Visuals = Window:AddTab("Tầm nhìn"),
     Floor = Window:AddTab("Tự động"),
     ["UI Settings"] = Window:AddTab("UI Settings"),
-}
-local FrameTimer = tick()
-local FrameCounter = 0;
-local FPS = 60;
-
-local WatermarkConnection = game:GetService('RunService').RenderStepped:Connect(function()
-	FrameCounter += 1;
-
-	if (tick() - FrameTimer) >= 1 then
-		FPS = FrameCounter;
-		FrameTimer = tick();
-		FrameCounter = 0;
-	end;
-
-	Library:SetWatermark(('Dương-Api | %s fps | %s ms'):format(
-		math.floor(FPS),
-		math.floor(game:GetService('Stats').Network.ServerStatsItem['Data Ping']:GetValue())
-	));
-end)				
+}		
 
 --// Captions \\--
 local _mspaint_custom_captions = Instance.new("ScreenGui") do
