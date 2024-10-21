bf1675aa855598fd4f755242e9218be036e50774
laitung1122 <137845259+laitung1122@users.noreply.github.com>
2024-10-21 20:50:57 +0700

    Update l2.lua

--- duong/l2.lua
+++ duong/l2.lua
@@ -127,11 +127,6 @@ local SaveManager = loadstring(game:HttpGet(repo .. "addons/SaveManager.lua"))()
 local Options = getgenv().Linoria.Options
 local Toggles = getgenv().Linoria.Toggles
 
-game:GetService("StarterGui"):SetCore("SendNotification",{
-Title = "Chào mừng đến với Dương Api!",
-Text = "Cảm ơn vì đã sài dự án", 
-Duration = 5
-})
 local Window = Library:CreateWindow({
     Title = "Dương Api | DOORS (Sảnh)",
     Center = true,
